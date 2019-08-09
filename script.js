ae029fc6faf49e6dedabb36cefd3b0d6a2ceb59d
Thersis94 <thersis94@gmail.com>
2019-08-08 21:09:44 -0600

    added checkbox inputs

--- script.js
+++ script.js
@@ -32,21 +32,19 @@ function fetchDrinkAPI(userDrinkSelect) {
     return fetch(`https://www.thecocktaildb.com/api/json/v2/8673533/lookup.php?i=${userDrinkSelect}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             data.drinks.map(function (item) {
                 const itemPic = item.strDrinkThumb
                 const itemName = item.strDrink
                 const itemGlass = item.strGlass
                 const itemIng = [item.strIngredient1, item.strIngredient2, item.strIngredient3, item.strIngredient4, item.strIngredient5, item.strIngredient6, item.strIngredient7, item.strIngredient8, item.strIngredient9, item.strIngredient10, item.strIngredient11, item.strIngredient12, item.strIngredient13, item.strIngredient14, item.strIngredient15].filter(Boolean).join(", ")
                 const itemInst = item.strInstructions
-                console.log(itemIng)
                 renderSelectedItem(itemPic, itemName, itemGlass, itemIng, itemInst)
             })
         })
         .catch(err => console.log(err))
 }
 
-function renderSelectedItem(itemPic, itemName, itemGlass, itemIng, itemInst){
+function renderSelectedItem(itemPic, itemName, itemGlass, itemIng, itemInst) {
     $(".drink-page").append(
         `
         <p class="drink-name">${itemName}</p>
@@ -63,9 +61,14 @@ function renderSelectedItem(itemPic, itemName, itemGlass, itemIng, itemInst){
 $("form").on("submit", function (event) {
     event.preventDefault()
     const userInput = $(event.currentTarget).find("#user-input").val().split(", ").join(",")
-    console.log(userInput)
+    let userHighLight = []
+    $(event.currentTarget).find("input[name='spirits']:checked").each(function () {
+        userHighLight.push(this.value)
+    })
+    userHighLight = userHighLight.join(",")
+    console.log(userHighLight + "," + userInput)
     STORE.currentVIEW = 'Results'
-    fetchAPI(userInput)
+    fetchAPI(userHighLight + "," + userInput)
     renderNextPage()
 })
 
@@ -84,14 +87,27 @@ function fetchAPI(userInput) {
         .then(res => res.json())
         .then(data => {
             console.log(data.drinks)
-            data.drinks.map(function (item) {
-                const itemPic = item.strDrinkThumb
-                const itemName = item.strDrink
-                const itemId = item.idDrink
-                renderResultItems(itemPic, itemName, itemId)
-            })
+            if (data.drinks === "None Found") {
+                renderError()
+            }
+            else {
+                data.drinks.map(function (item) {
+                    const itemPic = item.strDrinkThumb
+                    const itemName = item.strDrink
+                    const itemId = item.idDrink
+                    renderResultItems(itemPic, itemName, itemId)
+                })
+            }
         })
         .catch(err => console.log(err))
+}
+
+function renderError() {
+    $(".search-results").append(
+        `
+        <p class="error">No drinks were found!</p>
+        `
+    )
 }
 
 function renderResultItems(itemPic, itemName, itemId) {
@@ -99,7 +115,7 @@ function renderResultItems(itemPic, itemName, itemId) {
         `
         <span class="drink-span" data-id="${itemId}">
         <p class="results-drink-name">${itemName}</p>
-        <img class="results-drink-pic" src=${itemPic}>
+        <img alt="${itemName}" class="results-drink-pic" src=${itemPic}>
         </span>
         `
     )
